8268b2d148e0555256517d5110d3f1109139c64e
Ea123 <96218748+DuyEa@users.noreply.github.com>
2024-06-12 14:13:32 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import StudentDashboard from "./pages/StudentDashboard";
+import CourseManagement from "./pages/CourseManagement";
+import CourseEdit from "./pages/CouseEdit";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<StudentDashboard />} />
+        <Route path="/CourseManagement" element={<CourseManagement />} />
+        <Route path="/CourseEdit" element={<CourseEdit />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
-
