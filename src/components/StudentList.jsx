5ad1a761de4494aeca94ad079dcbaa7c63dd06fe
lucifer <d2f.working@gmail.com>
2024-06-14 19:22:35 +0700

    integrate api

--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -1,11 +1,10 @@
-import React, {useEffect, useState} from "react";
-import {Dialog, DialogPanel} from "@headlessui/react";
+import React, { useEffect, useState } from "react";
+import { Dialog, DialogPanel } from "@headlessui/react";
+import { useSelector, useDispatch } from "react-redux";
+import { createUser, fetchUsers } from "../redux/actions/userAction";
 import StudentCard from "./StudentCard";
 import StudentTable from "./StudentTable";
-import {useSelector, useDispatch} from 'react-redux';
-import {FileUpload} from "primereact/fileupload";
-import {createUser, fetchUsers} from "../redux/actions/userAction";
-import {CreateStudent} from "./CreateStudent";
+import { CreateStudent } from "./CreateStudent";
 
 const newClients = [
     {
@@ -36,11 +35,12 @@ const newClients = [
             "https://media.sproutsocial.com/uploads/2022/06/profile-picture.jpeg",
     },
 ];
+
 const StudentList = () => {
     const [isOpen, setIsOpen] = useState(false);
     const dispatch = useDispatch();
-    const students = useSelector(state => state.users.users);
-    console.log(students)
+    const students = useSelector((state) => state.users.users);
+
     useEffect(() => {
         dispatch(fetchUsers());
     }, [dispatch]);
@@ -48,6 +48,8 @@ const StudentList = () => {
     const handleCreateStudent = (newStudent) => {
         dispatch(createUser(newStudent));
         setIsOpen(false);
+        // Optionally, you may want to update the student list immediately after creation
+        // dispatch(fetchUsers());
     };
 
     return (
@@ -55,10 +57,7 @@ const StudentList = () => {
             <div className="flex justify-end items-center space-x-4 mb-4">
                 {/* Icons and Profile Picture */}
             </div>
-            <div className="flex justify-end space-x-4 mb-4 items-center">
-                {/* Icons and Profile Picture */}
-            </div>
-            <hr/>
+            <hr />
             <div className="mb-6 mt-[30px] flex justify-between items-center">
                 <div>
                     <h2 className="text-[#171A1FFF] text-[32px] leading-[48px] font-sans font-extrabold">
@@ -78,43 +77,28 @@ const StudentList = () => {
                             placeholder="Search"
                             className="border border-gray-300 px-4 py-2 rounded-md bg-[#F3F4F6FF] pl-10"
                         />
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth="1.5"
-                            stroke="currentColor"
-                            className="absolute top-3 left-3 h-5 w-5 text-[#171A1FFF]"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-                            />
-                        </svg>
+                        {/* Search icon */}
                     </div>
                 </div>
             </div>
 
-            <div className="p-4 rounded-lg" style={{backgroundColor: "#F3F4FB"}}>
+            <div className="p-4 rounded-lg" style={{ backgroundColor: "#F3F4FB" }}>
                 <h3 className="text-[#323842FF] text-[18px] leading-[28px] font-sans font-normal mb-4">
                     New clients this week
                 </h3>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                     {newClients.map((client) => (
-                        <StudentCard key={client.id} client={client}/>
+                        <StudentCard key={client.id} client={client} />
                     ))}
                 </div>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                    {/*{students.map((student) => (*/}
-                    {/*    <StudentCard key={student?.id} client={student}/>*/}
-                    {/*))}*/}
+                    {/* Render actual students here */}
                 </div>
             </div>
-            <StudentTable students={students}/>
+            <StudentTable students={students} />
 
             {/* Dialog for creating a new student */}
-            <CreateStudent isOpen={ isOpen} setIsOpen={setIsOpen}/>
+            <CreateStudent isOpen={isOpen} setIsOpen={setIsOpen} onCreate={handleCreateStudent} />
         </div>
     );
 };
