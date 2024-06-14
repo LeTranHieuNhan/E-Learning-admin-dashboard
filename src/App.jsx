fe27cb30da305e53aeb37d200b3caa6b3f5ea326
Ea123 <96218748+DuyEa@users.noreply.github.com>
2024-06-14 19:25:10 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import {Routes, Route, BrowserRouter} from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import StudentDashboard from "./pages/StudentDashboard";
 import CourseManagement from "./pages/CourseManagement";
 import CourseEdit from "./pages/CouseEdit";
+import CourseHome from "./pages/CourseHome";
+import CourseDetail from "./components/CourseDetail";
 
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/StudentDashboard" element={<StudentDashboard/>}/>
-                <Route path="/" element={<StudentDashboard/>}/>
-                <Route path="/CourseManagement" element={<CourseManagement/>}/>
-                <Route path="/CourseEdit" element={<CourseEdit/>}/>
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<StudentDashboard />} />
+        <Route path="/CourseManagement" element={<CourseManagement />} />
+        <Route path="/CourseEdit" element={<CourseEdit />} />
+        <Route path="/CourseHome/:courseName" element={<CourseHome />} />
+        <Route path="/CourseDetail/:courseName" element={<CourseDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
