49d82a86c519adfdc9e04f90fb170e7c309b06b8
Ea123 <96218748+DuyEa@users.noreply.github.com>
2024-06-14 19:25:46 +0700

    Update14/06

---
+++ src/pages/CourseHome.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Sidebar from "../components/Sidebar";
+import CourseDetail from "../components/CourseDetail";
+
+function CourseHome() {
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <div className="flex-1 flex flex-col items-center">
+        <CourseDetail />
+      </div>
+    </div>
+  );
+}
+
+export default CourseHome;
